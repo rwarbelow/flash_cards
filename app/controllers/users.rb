49779167f83518988078d7210e021875ece5a452
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 16:18:05 -0500

    create_user.erb finished

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,25 @@
 get '/join' do
   erb :create_user
 end
+
+
+
+post '/create_new_user' do
+  @user = User.new(params[:user])
+
+  if @user.save
+    redirect "/user_profile/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :create_user
+  end
+end
+
+
+
+get '/user_profile/:id' do
+  @user = User.find(params[:id])
+  
+  erb :user_profile
+
+end
