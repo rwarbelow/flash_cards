9b357b247c2dce61cb9fb0fc004d81e1b8f2e46b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-20 13:12:47 -0500

    Added Deck History to Start Page

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -2,3 +2,10 @@ get '/decks' do
   @decks = Deck.all
   erb :decks
 end
+
+get '/decks/:deck_id/:user_id' do
+  @deck = Deck.find_by_id(params[:deck_id])
+  @user = User.find_by_id(params[:user_id])
+  erb :start_page
+end
+
