461ddd23dd49a8e7fbae3235ac240d3acf0d235f
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 01:10:38 -0500

    Sessions
    Worked on routes and passing session info page to page up until the game

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,52 @@
 enable :sessions
 
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 get '/decks' do
+  @user = User.find(params[:user_id])
   @decks = Deck.all
   erb :decks
+end
+
+get '/decks/:user_id' do
+  @user = User.find(params[:user_id])
+  @decks = Deck.all
+  erb :decks
+end
+
+get '/join' do
+  erb :create_user
+end
+
+get '/join/:user_id' do
+  @user = User.find(params[:user_id])
+  redirect "/#{@user.id}"
+end
+
+post '/create_new_user' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/user_profile/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :create_user
+  end
+end
+
+get '/user_profile/:user_id' do
+  @user = User.find(params[:user_id])
+  if @user.id #== current_user.id
+    erb :user_profile
+  else
+    @error = "Page does not exist. Sorry, buddy... :("
+    erb :user_profile
+  end
+end
+
+get '/:user_id' do
+  @user = User.find(params[:user_id])
+  erb :index
 end
